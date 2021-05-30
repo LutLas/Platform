25c139de7f6c043059f4521011e1d105701cf85c
Lutaka Chihota <lutakachihota@gmail.com>
2021-05-30 15:04:40 +0200

    Enforcing HTTPS Requests

--- Startup.cs
+++ Startup.cs
@@ -1,206 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Platform.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration configService)
-        {
-            Configuration = configService;
-        }
-        private IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
+            services.Configure<CookiePolicyOptions>(it => {
+                it.CheckConsentNeeded = context => true;
+            });
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(it => {
+                it.IdleTimeout = TimeSpan.FromMinutes(30);
+                it.Cookie.IsEssential = true;
+            });
         }
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseStaticFiles();
-
-            app.UseStaticFiles(new StaticFileOptions {
-                FileProvider = new
-                    PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
-                RequestPath = "/files"
-            });
-
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+            app.UseMiddleware<ConsentMiddleware>();
+            app.UseSession();
             app.UseRouting();
 
-            app.UseMiddleware<LocationMiddleware>();
-
             app.Use(async (context, next) => {
-                string defaultDebug = config["Logging:LogLevel:Default"];
                 await context.Response
-                .WriteAsync($"The config setting is: {defaultDebug}\n");
-                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
-                await context.Response
-                .WriteAsync($"The env setting is: {environ}\n");
-                string wsID = Configuration["WebService:Id"];
-                string wsKey = Configuration["WebService:Key"];
-                await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
-                await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}\n\n");
+                .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
                 await next();
             });
-
 
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    logger.LogDebug("Response for / started");
-                    await context.Response.WriteAsync("Hello World!");
-                    logger.LogDebug("Response for / completed");
-                });
-            });
-        }
-    }
-}
 
-
-
-
-            /*services.Configure<MessageOptions>(options => {
-                options.CityName = "Albany";
-            });*/
-            /*services.Configure<RouteOptions>(opts => {
-                opts.ConstraintMap.Add("countryName",
-                typeof(CountryRouteConstraint));
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env*//*, IOptions<MessageOptions> msgOptions*//*)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            //app.UseMiddleware<Population>();
-            //app.UseMiddleware<Capital>();
-
-            app.UseRouting();
-
-            app.Use(async (context, next) => {
-                Endpoint end = context.GetEndpoint();
-                if (end != null)
-                {
+                endpoints.MapGet("/cookie", async context => {
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
                     await context.Response
-                    .WriteAsync($"{end.DisplayName} Selected \n");
-                }
-                else
-                {
-                    await context.Response.WriteAsync("No Endpoint Selected \n");
-                }
-                await next();
-            });
-
-            app.UseEndpoints(endpoints => {*//*
-
-                endpoints.MapGet("files/{filename}.{ext}", async context => {
-                    await context.Response.WriteAsync("Request Was Routed\n");
-                    foreach (var kvp in context.Request.RouteValues)
-                    {
-                        await context.Response
-                        .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-                    }
+                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
                 });
-                endpoints.MapGet("capital/{country:countryName}", Capital.Endpoint);
-                endpoints.MapGet("size/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+                endpoints.MapGet("clear", context => {
+                    context.Response.Cookies.Delete("counter1");
+                    context.Response.Cookies.Delete("counter2");
+                    context.Response.Redirect("/");
+                    return Task.CompletedTask;
+                });
+
                 endpoints.MapFallback(async context => {
-                    await context.Response.WriteAsync("Routed to fallback endpoint");
-                });*//*
-                endpoints.Map("{number:int}", async context => {
-                    await context.Response.WriteAsync("Routed to the int endpoint");
-                })
-                .WithDisplayName("Int Endpoint")
-                .Add(b => ((RouteEndpointBuilder)b).Order = 1);
-                endpoints.Map("{number:double}", async context => {
-                    await context.Response
-                    .WriteAsync("Routed to the double endpoint");
-                })
-                .WithDisplayName("Double Endpoint")
-                .Add(b => ((RouteEndpointBuilder)b).Order = 2);
-            });
-
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Terminal Middleware Reached");
-            });*/
-
-            /*app.UseMiddleware<LocationMiddleware>();*/
-
-            /*app.Use(async (context, next) => {
-                if (context.Request.Path == "/location")
-                {
-                    MessageOptions opts = msgOptions.Value;
-                    await context.Response
-                    .WriteAsync($"{opts.CityName}, {opts.CountryName}");
-                }
-                else
-                {
-                    await next();
-                }
-            });*/
-
-            /*app.Use(async (context, next) => {
-                await next();
-                await context.Response
-                .WriteAsync($"\nStatus Code: { context.Response.StatusCode}");
-            });
-
-            app.Use(async (context, next) => {
-                if (context.Request.Path == "/short")
-                {
-                    await context.Response
-                    .WriteAsync($"Request Short Circuited");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
-            app.Use(async (context, next) => {
-                if (context.Request.Method == HttpMethods.Get
-                && context.Request.Query["custom"] == "true")
-                {
-                    await context.Response.WriteAsync("Custom Middleware \n");
-                }
-                await next();
-            });
-
-            app.Map("/branch", branch => {
-                branch.Run(new QueryStringMiddleWare().Invoke);
-                branch.Run(async (context) => {
-                    await context.Response.WriteAsync($"Branch Middleware");
+                    await context.Response.WriteAsync("Hello World!");
                 });
             });
-
-            app.MapWhen(context => context.Request.Query.Keys.Contains("mapped"),
-               branch => {
-                branch.Use(async (context, next) => {
-                    await context.Response.WriteAsync($"MapWhen Middleware");
-                });
-            });
-
-            app.UseMiddleware<QueryStringMiddleWare>();*/
-        /*
+        }
     }
 }
-*/
